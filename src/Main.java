b5c741581ed283d48a0cbbb63f91bea94a6ca827
Shamil Zainetdinov <shamilzainetdinov@yandex.ru>
2022-07-13 22:28:18 +0700

    threads safety

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,24 @@ import java.net.Socket;
 
 public class Main {
 
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
+
     public static void main(String[] args) throws IOException {
         String host = "localhost";
         int port = 8080;
-        try(
-                Socket clientSocket = new Socket(host,port);
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+        try{
+                clientSocket = new Socket(host,port);
+                out = new PrintWriter(clientSocket.getOutputStream(),true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out.println("Shamil");
             String resp = in.readLine();
             System.out.println(resp);
+        }finally {
+            clientSocket.close();
+            in.close();
+            out.close();
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -7,18 +7,28 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
 
-        // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-        ServerSocket serverSocket = new ServerSocket(8080);
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public static void main(String[] args) throws IOException{
+            try {
+                // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+                serverSocket = new ServerSocket(8080);
+                clientSocket = serverSocket.accept(); // ждем подключения
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        System.out.println("New connection accepted");
+                System.out.println("New connection accepted");
 
-        final String name = in.readLine();
+                final String name = in.readLine();
 
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }finally{
+                serverSocket.close();
+                in.close();
+                out.close();
+            }
     }
 }
